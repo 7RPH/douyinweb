1fbc6d71dfe4f01fc3137d65fb2d99473d361011
7RPH <894304504@qq.com>
2021-06-18 21:54:40 +0800

    update

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request, session, redirect
 from db.dbhandle import *
+import json
 
 app = Flask(__name__)
 app.config['SESSION_TYPE'] = 'filesystem'
@@ -9,37 +10,10 @@ app.config['SECRET_KEY'] = os.urandom(24)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
-
-
-@app.route('/login_check', methods=['POST'])
-def login_check():
-    data = request.form
-    login_status = login(data)
-    if login_status:
-        session['login_check'] = '1'
-        session['phone'] = data['phone']
-        return {'msg': True}
-    else:
-        return {'msg': False}
-
-
-@app.route('/register_check', methods=['POST'])
-def register_check():
-    data = request.form
-    login_status = register(data)
-    if login_status:
-        session['login_check'] = '1'
-        session['phone'] = data['phone']
-        return {'msg': True}
-    else:
-        return {'msg': False}
-
-
-@app.route('/logout', methods=['POST'])
-def logout():
-    session.clear()
-    return redirect('/', code=301)
+    sex = getSex()
+    like = getLike()
+    num = getNum()
+    return render_template('index.html', **sex, **like, **num)
 
 
 if __name__ == '__main__':
--- db/dbhandle.py
+++ db/dbhandle.py
@@ -1,15 +1,25 @@
-#这是数据库操作部分的接口 请尽量返回字典或json
+# 这是数据库操作部分的接口
 import pymysql
 
 # 打开数据库连接
-db = pymysql.connect(host='localhost',
-                     user='root',
-                     password='root',
-                     database='douyin')
+db = pymysql.connect(host='47.242.248.7',
+                     user='scu_douyin',
+                     password='JSs3EXHt5mGD5DPy',
+                     database='app')
 cursor = db.cursor()
 
-#性别比例需要数据形如[{name: '男', value: 10}, {name: '女', value: 10}, {name: '未知', value: 30}]
 
-#视频点赞数需要数据形如[{name: '渗透测试', value: 播放量}],按照前十的播放量的从小往大的顺序返回
+# 性别比例需要数据
+def getSex():
+    return {'sexdata': [{"name": "男", "value": 1}, {"name": "女", "value": 145}, {"name": "未知", "value": 14}]}
 
-#
+
+# 视频点赞数,按照前十的播放量的从小往大的顺序返回
+def getLike():
+    return {'likename': ['乐色炸串', '乐色暑促', 'i春秋', 'ichunqiu', 'github', 'gitee', '渗透', '渗透测试', '测试', 'test'],
+            'likelist': sorted([114, 514, 191, 982, 11, 451, 419, 198, 101, 141])}
+
+
+# 爬取到的2020年每个月份的视频数量
+def getNum():
+    return {'num': [114, 514, 191, 981, 11, 451, 419, 198, 101, 145, 141, 919]}
